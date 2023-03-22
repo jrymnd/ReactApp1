327761ae0d53db6c72bb9d4e5e8777ab718d9cbb
J.G <jesseray90@gmail.com>
2023-03-21 23:24:12 -0700

    removed unused imports

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
-import logo from './logo.svg';
+
 import './App.css';
 import TeamList from './components/lists/TeamList';
-import Header from './components/headers/PlayerHeader';
 import TeamHeader from './components/headers/TeamHeader';
-import Footer from './components/footers/appFooter';
 
 
 export default function MyApp() {
